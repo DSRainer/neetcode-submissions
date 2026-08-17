4efc01efe9fdc788f6715cccc9d7bcaaa5cb611e
Rainer Rodrigues <princerainer333@gmail.com>
2026-08-17 20:04:06 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false
+
+        let seen = {}
+        for(const char of s){
+            seen[char] = (seen[char] || 0) + 1;
+        }
+        for(const char of t){
+            if(!seen[char]){
+                return false
+            }
+            seen[char]--
+        }
+        return true
+    }
+}
