c654720d57287245ee00c5ebdd6ad36708e5bf4c
Rainer Rodrigues <princerainer333@gmail.com>
2026-08-25 17:16:45 +0530

    Add: top-k-elements-in-list - submission-13

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-13.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        let seen = {}
+        for(const num of nums){
+            seen[num] = (seen[num] || 0) + 1
+        }
+        const buckets = Array.from({length: nums.length + 1}, () => [])
+        for(const key in seen){
+            const freq = seen[key]
+            buckets[freq].push(key)
+        }
+        let result = [];
+        for(let i = buckets.length - 1; i >= 0; i--){
+            for(const num of buckets[i]){
+                result.push(num)
+
+                if(result.length === k){
+                    return result
+                }
+            }
+        }
+    }
+}
